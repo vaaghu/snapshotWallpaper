ee79c849f9b06660de84abff7ba84061d78f1973
vaaghu <vignesh021102@gmail.com>
2023-12-30 00:05:58 +0530

    first prototype

--- index.js
+++ index.js
@@ -1,31 +1,49 @@
 const { createCanvas, loadImage } = require('canvas');
 const fs = require('fs');
 
-
+const randomRange = (start,end) => {
+  return Math.floor(Math.random() * (end - start)) + start
+}
+const binary = ()=> Math.random() > 0.5
 console.log(process.argv)
-if (process.argv.length < 4) {
+if (process.argv.length < 6) {
   console.log('Please provide both width and height as command-line arguments.');
   process.exit(1);
 }
 
 // Retrieve width and height from command-line arguments
-let width = parseInt(process.argv[2]);
-let height = parseInt(process.argv[3]);
-// width = 250;
-// height = 250
+let DIR = process.argv[2]
+let width = parseInt(process.argv[3]);
+let height = parseInt(process.argv[4]);
+let isDark = parseInt(process.argv[5]);
 
-console.log(width,height) 
+
 const canvas = createCanvas(width, height);
 const ctx = canvas.getContext('2d');
+let bufferCuberSize = randomRange(0,40)
+let cubeSize = 100 
+let maxbrightness = (isRemove=false) => { return isRemove?0:randomRange(isDark?20:100,isDark?150:240)}
+ctx.fillStyle = isDark?"black":"white"
+ctx.fillRect(0,0,width,height)
 
-for (let i = 0;i<10;i++){
-  ctx.fillStyle = 'red';
-  ctx.fillRect(50, 50, 100, 100);
+
+console.log(maxbrightness())
+Red = binary()
+Green = binary()
+Blue = binary()
+// Green = 0
+// Blue = 0
+// Red = 0
+console.log(Red && Green && Blue,Red,Green,Blue)
+if (Red && Green && Blue) Red = false
+for (let i = 0;i<1000;i++){
+  ctx.fillStyle = `rgba(${maxbrightness(Red)},${maxbrightness(Green)},${maxbrightness(Blue)},0.9)`;
+  ctx.fillRect((Math.random()*width)-(cubeSize/2), (Math.random()*height)-(cubeSize/2),cubeSize+Math.floor(Math.random()*bufferCuberSize),cubeSize+Math.floor(Math.random()*bufferCuberSize));
 }
 
 
 // Save the canvas as a PNG file
-const out = fs.createWriteStream('Default.png');
+const out = fs.createWriteStream(DIR+'/Default.png');
 const stream = canvas.createPNGStream();
 
 stream.pipe(out);
--- main.sh
+++ main.sh
@@ -5,26 +5,31 @@ display_info=$(xrandr | awk '/\*/ {print $1}')
 display_width=$(echo "$display_info" | cut -d 'x' -f 1)
 display_height=$(echo "$display_info" | cut -d 'x' -f 2)
 
-echo "Display Width: $display_width"
-echo "Display Height: $display_height"
-
-node index.js $display_width $display_height
+# echo "Display Width: $display_width"
+# echo "Display Height: $display_height"
 
 colorScheme=$(gsettings get org.gnome.desktop.interface color-scheme)
+
+
 #echo "$colorScheme"
 
 #fileURL="file:///usr/share/backgrounds/warty-final-ubuntu.png"
-script_dir=$(pwd)
+script_dir=$(dirname "$(readlink -f "$0")")
 relative_path="Default.png"
 
 fileURL="file://$script_dir/$relative_path"
-
+echo $script_dir/index.js
 if [ "$colorScheme" == "'prefer-dark'" ]; then
-  echo "DARK"
+  node $script_dir/index.js $script_dir $display_width $display_height 1
+  # echo "DARK"
   gsettings set org.gnome.desktop.background picture-uri-dark $fileURL
   echo "$(gsettings get org.gnome.desktop.background picture-uri-dark)"
 else
-  echo "LIGHT"
+  node $script_dir/index.js $script_dir $display_width $display_height 0
+  # echo "LIGHT"
   gsettings set org.gnome.desktop.background picture-uri $fileURL
-  echo "$(gsettings get org.gnome.desktop.background picture-uri)"
+  # echo "$(gsettings get org.gnome.desktop.background picture-uri)"
 fi
+
+sleep 10m
+$script_dir/main.sh
